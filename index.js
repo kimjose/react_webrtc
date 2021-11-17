4e9ac5ab237ccfeaa8dae91018d03ccd43acb313
Kimani Joseph <kimjose693@gmail.com>
2021-11-17 20:37:53 +0300

    Deploy to heroku

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ io.on('connection', (socket) => {
     socket.emit('me', socket.id)
 
     socket.on('disconnect', () =>{
-        socket.broadcast.emit("Call Ended")
+        socket.broadcast.emit("callended")
     })
 
     socket.on("calluser", ({userToCall, signalData, from, name}) => {
         io.to(userToCall).emit("calluser", {signal: signalData, from, name})
     })
 
-    socket.on("answerCall", (data) => {
+    socket.on("answercall", (data) => {
         io.to(data.to).emit("callaccepted", data.signal);
     })
 
